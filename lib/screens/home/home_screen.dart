208ec0023efc462b6e6f64de56fd06ec86726283
FirdausyTama <atama.firdausy@gmail.com>
2025-05-21 18:37:49 +0700

    membuat homescreen dan app bar

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:plantapp/constants.dart';
+import 'package:plantapp/screens/home/components/body.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Body(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: kPrimaryColor,
+      elevation: 0,
+      iconTheme: const IconThemeData(color: Colors.white),
+      leading: IconButton(
+        icon: SvgPicture.asset("assets/icons/menu.svg", color: Colors.white),
+        onPressed: () {}, 
+        ),
+    );
+  }
+}
