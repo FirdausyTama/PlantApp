d2df6df55001b231ba75f7c377572275afe1415d
FirdausyTama <atama.firdausy@gmail.com>
2025-05-21 22:27:07 +0700

    membuat card tanaman dan scroll kanan kiri

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:plantapp/constants.dart';
+import 'package:plantapp/screens/home/components/RecomendsPlant.dart';
 import 'package:plantapp/screens/home/components/header_with_searchbox.dart';
+import 'package:plantapp/screens/home/components/title_with_more_btn.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -12,6 +15,9 @@ class Body extends StatelessWidget {
       child: Column(
         children: <Widget>[
           HeaderWithSearchBox(size: size),
+          TitleWithMoreButton(title: "Recomended", press: () {}),
+          RecomendsPlant(),
+          TitleWithMoreButton(title: "Featured Plants", press: () {}),
         ],
       ),
     );
