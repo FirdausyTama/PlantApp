3edbaf98b6cf188c504e528fa09992f144488924
FirdausyTama <atama.firdausy@gmail.com>
2025-05-23 01:06:52 +0700

    membuat deskripsi detail

---
+++ lib/screens/details/components/body.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:plantapp/constants.dart';
+import 'package:plantapp/screens/details/components/icon_card.dart';
+import 'package:plantapp/screens/details/components/image_and_icons.dart';
+import 'package:plantapp/screens/details/components/title_and_price.dart';
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ImageAndIcons(size: size),
+          TitleAndPrice(title: "Mangga", country: "Indonesia", price: 440),
+          SizedBox(height: kDefaultPadding),
+          Row(
+            children: [
+              SizedBox(
+                width: size.width / 2,
+                height: 84,
+                child: FilledButton(
+                  style: FilledButton.styleFrom(
+                    backgroundColor: kPrimaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                      ), 
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "Buy Now",
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: FilledButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all<Color>(
+                      Colors.transparent,
+                    ),
+                  ),
+                  child: Text("Description", style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
